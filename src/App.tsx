884b42ff296d2cf8e08506df80d9feaff8d30f2f
Sofia A <alvazzi.sofia@gmail.com>
2026-01-08 23:51:06 -0500

    changed router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import HorizontalNavbar from './components/HorizontalNavbar';
 import VerticalNavbar from './components/VerticalNavbar';
 import AboutMeSection from './components/AboutMeSection';
